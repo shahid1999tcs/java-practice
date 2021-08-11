ee11e579f0c358104ee54ec1f4ac94db05bda869
shahid1999tcs <hellohbk29@gmail.com>
2021-08-11 06:34:39 +0000

    static

---
+++ src/statickeyword/StaticBlockDemo.java
@@ -0,0 +1,25 @@
+package statickeyword;
+
+public class StaticBlockDemo {
+	private static int age = 10;
+	int price = 20; // cannot be accessed/referred inside static block/methods
+
+	public StaticBlockDemo() {
+		System.out.println("in constructor..");
+	}
+
+	static {// called first, loaded during startup
+		final int localVariable = 12; //Only final variable can be defined
+		System.out.println("this is static block" + age);
+	}
+
+	public static void main(String[] args) {
+//		StaticBlockDemo blockDemo = new StaticBlockDemo();
+//		new StaticBlockDemo();
+
+	}
+
+	void work() {
+//		static int time = 45; //local variable cannot be static
+	}
+}
