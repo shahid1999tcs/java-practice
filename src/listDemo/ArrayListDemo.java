b7e99b9170327ca73b7a89f2dc8d60a0d987fb23
shahid1999tcs <hellohbk29@gmail.com>
2021-08-12 06:36:15 +0000

    Array operations

--- src/listDemo/ArrayListDemo.java
+++ src/listDemo/ArrayListDemo.java
@@ -3,26 +3,46 @@ package listDemo;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ArrayListDemo {
 	public static void main(String[] args) {
 		List<Integer> numbers = createList();
-		//sortList(a);
-		filterList(numbers);
+		// sortList(a);
+		//filterList(numbers);
+		//add(numbers);
+		otherOps(numbers);
 	}
+
+	private static void otherOps(List<Integer> numbers) {
+		// TODO Auto-generated method stub
+		System.out.println(numbers);
+		//numbers.remove(0);
+		numbers.add(1,20);
+		System.out.println(numbers);
+	}
+
+	private static void add(List<Integer> numbers) {
+		// TODO Auto-generated method stub
+		Optional<Integer> sum = numbers.stream().reduce((Integer num1, Integer num2) -> {
+			System.out.println(num1 + ", "+ num2);
+			return num1 + num2;
+		});
+		System.out.println(sum);
+	}
+
 	/**
 	 * Method to filter
+	 * 
 	 * @param numbers
 	 */
 	private static void filterList(List<Integer> numbers) {
 		// TODO Auto-generated method stub
-		numbers.forEach((number)-> {
-			System.out.println(number%2==0);
+		numbers.forEach((number) -> {
+			System.out.println(number % 2 == 0);
 		});
-		List<Integer> filtered = numbers.stream()
-				.filter((number) -> number%2 == 0)
-				.collect(Collectors.toList());
+		List<Integer> filtered = numbers.stream().filter((number) -> number % 2 == 0).collect(Collectors.toList());
 		System.out.println(filtered);
 	}
 
